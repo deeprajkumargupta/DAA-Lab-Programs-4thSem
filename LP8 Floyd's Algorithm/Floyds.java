5d7593d6965e3affd58e0b809474b67ce25d5b0f
deeprajkumargupta <192437937+deeprajkumargupta@users.noreply.github.com>
2026-05-18 22:02:57 +0530

    PGM8 (Flord's Algorithm)

---
+++ LP8 Floyd's Algorithm/Floyds.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class Floyds {
+    static void floyd(int D[][], int n) {
+        for (int k = 0; k < n; k++) {
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    D[i][j] = min(D[i][j], D[i][k] + D[k][j]);
+                }
+            }
+        }
+    }
+
+    static int min(int a, int b) {
+        return (a < b ? a : b);
+    }
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter no. of Stations");
+        int n = s.nextInt();
+        int[][] cost = new int[n][n];
+        // Enter the cost matrix, 0 for principle diagnol axis and no direct edge
+        // (all source shortest path, source to source is zero)
+        // Make 999 for no direct edge from i to j
+        System.out.println("Enter the travel time between subway lines");
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                cost[i][j] = s.nextInt();
+                if (cost[i][j] == 0 && i != j)
+                    cost[i][j] = 999;
+            }
+        }
+        floyd(cost, n);
+        System.out.println("Shortest Travel Time between all stations");
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++)
+                System.out.print(cost[i][j] + "  ");
+            System.out.println();
+        }
+    }
+}
